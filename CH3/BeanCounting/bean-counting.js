f456dc63b1b0ea76b29f2a2d2e69d9660a4d2785
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-07-08 21:22:19 -0400

    complete bean-counting part 2

--- CH3/BeanCounting/bean-counting.js
+++ CH3/BeanCounting/bean-counting.js
@@ -8,7 +8,17 @@ const countBs = (string) => {
   return Bs
 }
 
+const countChar = (string, char) => {
+  let charCount = 0
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === char) {
+      charCount++
+    }
+  }
+  return charCount
+}
+
 console.log(countBs("BBC"));
 // → 2
-// console.log(countChar("kakkerlak", "k"));
+console.log(countChar("kakkerlak", "k"));
 // → 4
